27702b2b22cea1d441631a6bff4fb7c7e6240238
GIS-sys <67117384+GIS-sys@users.noreply.github.com>
2025-11-14 06:01:52 +0300

    add bars drawing

--- source/render.cpp
+++ source/render.cpp
@@ -38,54 +38,53 @@ void render_world(SDL_Window *window, SDL_Renderer *renderer, World &world)
     for (int i = 0; i < world.currentHeroes.size(); ++i)
         render_sprite(&world.currentHeroes.sprites[i], &world.currentHeroes.transform2ds[i]);
 
-    // // Draw bars without textures and without OOP
-    // float grayColor[4] = {0.2f, 0.2f, 0.2f, 1.f};
-    // float healthColor[4] = {0.91f, 0.27f, 0.22f, 1.f};
-    // float staminaColor[4] = {0.f, 0.6f, 0.86f, 1.f};
+    // Draw bars without textures and without OOP
+    float grayColor[4] = {0.2f, 0.2f, 0.2f, 1.f};
+    float healthColor[4] = {0.91f, 0.27f, 0.22f, 1.f};
+    float staminaColor[4] = {0.f, 0.6f, 0.86f, 1.f};
 
-    // std::vector<SDL_FRect> backBars;
-    // std::vector<SDL_FRect> healthBars;
-    // std::vector<SDL_FRect> staminaBars;
-    // for (const auto &object : world.get_objects())
-    // {
-    //     auto transform = object->get_component<Transform2D>();
-    //     auto health = object->get_component<Health>();
-    //     auto stamina = object->get_component<Stamina>();
-    //     if (!transform)
-    //         continue;
-    //     if (health)
-    //     {
-    //         Transform2D barTransform = *transform;
-    //         barTransform.sizeX *= 0.1f;
-    //         SDL_FRect dst = to_camera_space(barTransform, *camera_transform, *camera2d);
-    //         dst.x += screenW / 2;
-    //         dst.y += screenH / 2;
-    //         backBars.push_back(dst);
-    //         const float value = float(health->current) / float(health->max);
-    //         dst.y += (1.f - value) * dst.h;
-    //         dst.h *= value;
-    //         healthBars.push_back(dst);
-    //     }
-    //     if (stamina)
-    //     {
-    //         Transform2D barTransform = *transform;
-    //         barTransform.x += barTransform.sizeX * 0.9f;
-    //         barTransform.sizeX *= 0.1f;
-    //         SDL_FRect dst = to_camera_space(barTransform, *camera_transform, *camera2d);
-    //         dst.x += screenW / 2;
-    //         dst.y += screenH / 2;
-    //         backBars.push_back(dst);
-    //         const float value = float(stamina->current) / float(stamina->max);
-    //         dst.y += (1.f - value) * dst.h;
-    //         dst.h *= value;
-    //         staminaBars.push_back(dst);
-    //     }
-    // }
+    std::vector<SDL_FRect> backBars;
+    std::vector<SDL_FRect> healthBars;
+    std::vector<SDL_FRect> staminaBars;
+    auto render_bars = [&](Transform2D *transform, Health *health, Stamina *stamina)
+    {
+        if (health)
+        {
+            Transform2D barTransform = *transform;
+            barTransform.sizeX *= 0.1f;
+            SDL_FRect dst = to_camera_space(barTransform, camera_transform, camera2d);
+            dst.x += screenW / 2;
+            dst.y += screenH / 2;
+            backBars.push_back(dst);
+            const float value = float(health->current) / float(health->max);
+            dst.y += (1.f - value) * dst.h;
+            dst.h *= value;
+            healthBars.push_back(dst);
+        }
+        if (stamina)
+        {
+            Transform2D barTransform = *transform;
+            barTransform.x += barTransform.sizeX * 0.9f;
+            barTransform.sizeX *= 0.1f;
+            SDL_FRect dst = to_camera_space(barTransform, camera_transform, camera2d);
+            dst.x += screenW / 2;
+            dst.y += screenH / 2;
+            backBars.push_back(dst);
+            const float value = float(stamina->current) / float(stamina->max);
+            dst.y += (1.f - value) * dst.h;
+            dst.h *= value;
+            staminaBars.push_back(dst);
+        }
+    };
+    for (int i = 0; i < world.currentEnemies.size(); ++i)
+        render_bars(&world.currentEnemies.transform2ds[i], &world.currentEnemies.healths[i], &world.currentEnemies.staminas[i]);
+    for (int i = 0; i < world.currentHeroes.size(); ++i)
+        render_bars(&world.currentHeroes.transform2ds[i], &world.currentHeroes.healths[i], &world.currentHeroes.staminas[i]);
 
-    // SDL_SetRenderDrawColorFloat(renderer, grayColor[0], grayColor[1], grayColor[2], grayColor[3]);
-    // SDL_RenderFillRects(renderer, backBars.data(), int(backBars.size()));
-    // SDL_SetRenderDrawColorFloat(renderer, healthColor[0], healthColor[1], healthColor[2], healthColor[3]);
-    // SDL_RenderFillRects(renderer, healthBars.data(), int(healthBars.size()));
-    // SDL_SetRenderDrawColorFloat(renderer, staminaColor[0], staminaColor[1], staminaColor[2], staminaColor[3]);
-    // SDL_RenderFillRects(renderer, staminaBars.data(), int(staminaBars.size()));
+    SDL_SetRenderDrawColorFloat(renderer, grayColor[0], grayColor[1], grayColor[2], grayColor[3]);
+    SDL_RenderFillRects(renderer, backBars.data(), int(backBars.size()));
+    SDL_SetRenderDrawColorFloat(renderer, healthColor[0], healthColor[1], healthColor[2], healthColor[3]);
+    SDL_RenderFillRects(renderer, healthBars.data(), int(healthBars.size()));
+    SDL_SetRenderDrawColorFloat(renderer, staminaColor[0], staminaColor[1], staminaColor[2], staminaColor[3]);
+    SDL_RenderFillRects(renderer, staminaBars.data(), int(staminaBars.size()));
 }
