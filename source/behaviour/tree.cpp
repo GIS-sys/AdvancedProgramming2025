4681156b851d73231d443dc2b316bd84f4a2c293
GIS-sys <67117384+GIS-sys@users.noreply.github.com>
2025-11-21 00:46:15 +0300

    done beh tree

--- source/behaviour/tree.cpp
+++ source/behaviour/tree.cpp
@@ -21,7 +21,7 @@ void BehaviourTree::buildTree()
     {
         rootNode = new BehaviourTreeNodeCondition(
             [](BehaviourUpdateData data) -> bool
-            { return data.health.current > 90; },
+            { return data.health.current < 90; },
 
             new BehaviourTreeNodeConsequitive({
                 new BehaviourTreeNodeDebug("PREDATOR MATE"),
@@ -50,7 +50,7 @@ void BehaviourTree::buildTree()
                         currentTarget = findClosestFoodConsumerOrHero(data.world, data.currentPos, data.pathfinder);
                         return true;
                     },
-                    {0.9f, 0.3f, 0.5f}),
+                    {0.3f, 0.5f, 0.5f}),
                 new BehaviourTreeNodeAction(
                     [](BehaviourUpdateData data, int2 &currentTarget) -> bool
                     {
@@ -58,12 +58,78 @@ void BehaviourTree::buildTree()
                             return true;
                         return data.currentPos == currentTarget;
                     },
-                    {0.9f, 0.3f, 0.5f}),
+                    {0.3f, 0.5f, 0.5f}),
             }));
     }
     else
     {
-        rootNode = new BehaviourTreeNodeDebug("CONSUMER");
+        rootNode = new BehaviourTreeNodeCondition(
+            [](BehaviourUpdateData data) -> bool
+            { return !hasPredatorNearby(data.world, data.currentPos); },
+
+            new BehaviourTreeNodeConsequitive({
+                new BehaviourTreeNodeDebug("CONSUMER AVOID"),
+                new BehaviourTreeNodeAction(
+                    [](BehaviourUpdateData data, int2 &currentTarget) -> bool
+                    {
+                        currentTarget = findOppositeToClosestPredator(data.world, data.currentPos, data.pathfinder, data.restrictor);
+                        return true;
+                    },
+                    {0.9f, 0.3f, 0.3f}),
+                new BehaviourTreeNodeAction(
+                    [](BehaviourUpdateData data, int2 &currentTarget) -> bool
+                    {
+                        if (!hasPredatorNearby(data.world, data.currentPos))
+                            return true;
+                        return data.currentPos == currentTarget;
+                    },
+                    {0.9f, 0.3f, 0.3f}),
+            }),
+
+            new BehaviourTreeNodeCondition(
+                [](BehaviourUpdateData data) -> bool
+                { return data.health.current < 90; },
+
+                new BehaviourTreeNodeConsequitive({
+                    new BehaviourTreeNodeDebug("CONSUMER MATE"),
+                    new BehaviourTreeNodeAction(
+                        [](BehaviourUpdateData data, int2 &currentTarget) -> bool
+                        {
+                            currentTarget = findClosestPredator(data.world, data.currentPos, data.pathfinder);
+                            return true;
+                        },
+                        {0.6f, 0.3f, 0.5f}),
+                    new BehaviourTreeNodeAction(
+                        [](BehaviourUpdateData data, int2 &currentTarget) -> bool
+                        {
+                            if (data.health.current < 80 || data.stamina.current < 50)
+                                return true;
+                            return data.currentPos == currentTarget;
+                        },
+                        {0.6f, 0.3f, 0.5f}),
+                }),
+
+                new BehaviourTreeNodeConsequitive({
+                    new BehaviourTreeNodeDebug("CONSUMER FEED"),
+                    new BehaviourTreeNodeAction(
+                        [this](BehaviourUpdateData data, int2 &currentTarget) -> bool
+                        {
+                            if (data.health.current < data.stamina.current)
+                                currentTarget = findClosestFoodHealth(data.world, data.currentPos, data.pathfinder);
+                            else
+                                currentTarget = findClosestFoodStamina(data.world, data.currentPos, data.pathfinder);
+                            return true;
+                        },
+                        {0.3f, 0.7f, 0.7f}),
+                    new BehaviourTreeNodeAction(
+                        [](BehaviourUpdateData data, int2 &currentTarget) -> bool
+                        {
+                            if (data.health.current > 90)
+                                return true;
+                            return data.currentPos == currentTarget;
+                        },
+                        {0.3f, 0.7f, 0.7f}),
+                })));
     }
 }
 
